06979cdd87647221b118fb27a12a2ed930c2e52b
Cecilia Humlelu <cecilia.humlelu@screeninteraction.com>
2014-07-07 10:03:00 +0200

    First commit

---
+++ Assets/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public Transform target;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(transform.position.x,transform.position.y,target.transform.position.z-10.0f);
+	}
+
+
+
+		
+	}
---
+++ Assets/CubePlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubePlayerMovement : MonoBehaviour {
+
+	public float aSpeed = 3.0f;
+	public Animation cubeAnimation;
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(0, 0, Time.deltaTime * aSpeed);
+		print ("trasnform position" + transform.position);
+		jump ();
+	}
+
+	void jump(){
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			cubeAnimation.Play("JumpUpAnimation");
+		}
+	}
+}
